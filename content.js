d56e8a2e168ba14873a2eebc88b7278fcd567dfb
devinS15 <devin.strong@speeracademy.org>
2017-05-26 13:14:36 -0500

    v.0.1.4.0 added an icon for my popup window

--- content.js
+++ content.js
@@ -1,1 +1,11 @@
-ke
+"content_scripts": [
+  {
+    "matches": [
+      "<all_urls>"
+    ],
+    "js": ["content.js"]
+    var firstHref = $("a[href^='http']").eq(0).attr("href");
+
+console.log(firstHref);
+  }
+]
